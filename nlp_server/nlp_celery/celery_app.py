592babaea3b0063be08531a95e6b2747e3d50b81
Andrew Evans <asevans48@gmail.com>
2019-08-26 22:00:59 -0600

    testing application

--- nlp_server/nlp_celery/celery_app.py
+++ nlp_server/nlp_celery/celery_app.py
@@ -12,15 +12,48 @@ Options:
 """
 
 from celery import Celery
+from docopt import docopt
+import json
 
+from nlp_server.cache import cache_ops
 from nlp_server.nlp_celery.ner_task import NERTask
 from nlp_server.nlp_celery.sent_tokenizer_task import SentenceTokenizer
 from nlp_server.nlp_celery.topic_tiler_task import TopicTokenizer
 
 
+def set_config(doc, client):
+    """
+    Set the config path
+
+    :param doc: The document path
+    :param client:  The memcached config
+    """
+    if doc.get('CONFIGPATH'):
+        cfg_path = doc.get('CONFIGPATH')
+        with open(cfg_path, 'r') as fp:
+            cfg = dict(json.load(fp))
+
+        if cfg and cfg.get('ner'):
+            ner_jsn = json.dumps(cfg.get('ner'))
+            client.set('ner_config', ner_jsn)
+
+        if cfg and cfg.get('sent_tokenizer'):
+            sent_jsn = json.dumps(cfg.get('sent_tokenizer'))
+            client.set('sent_tokenizer_config', sent_jsn)
+
+        if cfg and cfg.get('topic_segmentation'):
+            topic_jsn = json.dumps(cfg.get('text_segmentation'))
+            client.set('topic_tiler_config', topic_jsn)
+
+
 if __name__ == "__main__":
-    app = Celery('nlp_server')
-    app.register_task(NERTask)
-    app.register_task(SentenceTokenizer)
-    app.register_task(TopicTokenizer)
-    app.worker_main()
+    APP = Celery('nlp_server')
+
+    DOC = docopt(__doc__, version='NLP Server 0.1')
+    CLIENT = cache_ops.get_memcache()
+    set_config(DOC, CLIENT)
+
+    APP.register_task(NERTask)
+    APP.register_task(SentenceTokenizer)
+    APP.register_task(TopicTokenizer)
+    APP.worker_main()
